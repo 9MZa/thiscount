66a2ad97ca3c4c0693aaac4eee9f85c8a73ad5da
Bandit Silachai <mbandit001@yahoo.co.th>
2022-08-19 14:29:25 +0700

    📇 use next-seo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,23 +1,36 @@
+import { Box } from "@mantine/core";
+import Navbar from "./Navbar";
+import { NextSeo } from "next-seo";
+import img from "../public/images.jpeg";
+
 interface LayoutProps {
   children: React.ReactNode;
   title?: string;
 }
 
-import { Box } from "@mantine/core";
-import Navbar from "./Navbar";
-import Head from "next/head";
-
-const Layout = ({ children, title = "iCal" }: LayoutProps): JSX.Element => {
+const Layout = ({ children, title = "iCalc" }: LayoutProps): JSX.Element => {
   return (
     <>
-      <Head>
-        <title>{`${title}`}</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
+      <NextSeo
+        title={title}
+        description="คิด % และ VAT ง่ายๆ ผ่านเว็บไซต์"
+        openGraph={{
+          url: "https://icalc.vercel.app/",
+          title: title,
+          description: "คิด % และ VAT ง่ายๆ ผ่านเว็บไซต์",
+          locale: "th-TH",
+          images: [
+            {
+              url: img.src,
+              width: 800,
+              height: 600,
+              alt: "Calculator",
+              type: "image/jpeg",
+            },
+          ],
+          site_name: "SiteName",
+        }}
+      />
       <Navbar />
       <Box>{children}</Box>
     </>
